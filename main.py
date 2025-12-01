66479b61a8b554b65ca60da0b14925f7703eff7e
Xibsy <pro45951@gmail.com>
2025-12-01 16:27:33 +0500

    End maybe =)

--- main.py
+++ main.py
@@ -1,17 +1,36 @@
-from constants import RPC
+from pypresence import Presence
 from pypresence.types import ActivityType
-from time import sleep
+from pypresence.exceptions import DiscordNotFound
+from time import sleep, time
 
 
-RPC.connect()
+client_id = '1442081639436914709'
+RPC = Presence(client_id)
+LISTENING_BUTTONS = [
+    {
+        'label': 'My GitHub',
+        'url': 'https://github.com/Xibsy'
+    },
+    {
+        'label': 'Telegram',
+        'url': 'https://t.me/Xibsy'
+    }]
 
-RPC.update(
-    activity_type=ActivityType.LISTENING,
-    details='Ого рпс рабитает',
-    state='Работаю над проектом',
-    state_url="https://github.com/Xibsy/MyDiscordRpc",
-    end=60000
-)
+RPC.connect()  # Start the handshake loop
 
 while True:
+    try:
+        RPC.update(
+            activity_type=ActivityType.WATCHING,
+            details="Хай Я Xibsy",
+            state="https://guns.lol/Xibsy",
+            name="на move.move",
+            start=time(),
+            large_image='non-active',
+            buttons=LISTENING_BUTTONS,
+
+        )
+    except DiscordNotFound:
+            print('биля')
+    # The presence will stay on as long as the program is running
     sleep(15)
